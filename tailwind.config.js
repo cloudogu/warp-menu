b100e4fb1468f9b12791781b239d7fa91974a791
Simon Klein <simon.klein@cloudogu.com>
2024-08-16 07:37:52 +0200

    #49 Add screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,4 +19,15 @@ module.exports = {
             addBase(scopedUtilities);
         }),
     ],
+    theme: {
+        extend: {
+            screens: {
+                'warp-lg': '897px',
+                'not-warp-lg': {'max': '896px'},
+                'warp-md': {'max': '896px', 'min': '701px'},
+                'warp-sm': {'max': '700px', 'min': '471px'},
+                'warp-xs': {'max': '470px'},
+            }
+        }
+    }
 }
